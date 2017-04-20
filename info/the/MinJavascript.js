b503a6a09093ef49cff74fa25f3623b144793ebb
laban78 <elev.jonathan.larsson2@skola.lund.se>
2017-04-20 11:04:22 +0200

    Add files via upload

---
+++ info/the/MinJavascript.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+/*
+function openNav(){
+	document.getElementById("myNavbar").style.display="block";
+	document.getElementById("openNav").style.display="none";
+	document.getElementById("closeNav").style.display="block";	
+}
+function closeNav(){
+	document.getElementById("myNavbar").style.display="none";
+	document.getElementById("openNav").style.display="block";	
+    document.getElementById("closeNav").style.display="none";	
+}*/
+		$(document).ready(function() {
+
+			/*
+			* Plugin intialization
+			*/
+	    	$('#pagepiling').pagepiling({
+	    		menu: '#menu',
+	    		anchors: ['page1', 'page2', 'page3', 'page4'],
+			    sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C'],
+			    navigation: {
+			    	'position': 'right',
+			   		'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
+			   	},
+			    afterRender: function(){
+			    	$('#pp-nav').addClass('custom');
+			    },
+			    afterLoad: function(anchorLink, index){
+			    	if(index>1){
+			    		$('#pp-nav').removeClass('custom');
+			    	}else{
+			    		$('#pp-nav').addClass('custom');
+			    	}
+			    }
+			});
+
+
+			/*
+		    * Internal use of the demo website
+		    */
+		    $('#showExamples').click(function(e){
+				e.stopPropagation();
+				e.preventDefault();
+				$('#examplesList').toggle();
+			});
+
+			$('html').click(function(){
+				$('#examplesList').hide();
+			});
+	    });
+
+
+
+
+
+
+
+
